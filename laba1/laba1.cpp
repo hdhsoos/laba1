3219c2ad8807c77c830da3819702343231524172
Мария Андреева <AndreevaMM@stud.kai.ru>
2022-02-16 19:29:32 +0300

    Расписала исключения

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -2,20 +2,41 @@
 #include <fstream>
 
 using namespace std;
-void fail() {
+
+int isdi(char s[]) {
+    int i{};
+    for (i = 0; i < 256; i++) {
+        if (!isdigit(s[i])) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int fail() {
     cout << "Хорошо, читаю файл." << endl;
     ifstream fin("1laba.txt");
     if (!fin.is_open())
         cout << "Файл невозможно открыть. Завершаю работу." << endl;
     else {
-        char a1[15]{}, a2[15]{}; // Если будем использовать double,
-                             // число будет сильно округляться. 
-                             // Поэтому удобнее сохранить хотя бы первые 15 знаков.
+        char a1[256]{}, a2[256]{}; // Если будем использовать double,
+                                   // число будет сильно округляться. 
         int i{};
         fin >> a1;
+        bool isd;
+        isd = isdi(a1);
+        if (isd == 1) {
+            cout << "Ошибка ввода. Завершаю работу." << endl;
+            return 0;
+        }
         ofstream fout("1labaout.txt");
         for (i = 0; i < 69; i++) {
             fin >> a2;
+            isd = isdi(a2);
+            if (isd == 1) {
+                cout << "Ошибка ввода. Завершаю работу." << endl;
+                return 0;
+            }
             fout << a2;
             fout << " ";
             cout << a2;
@@ -25,17 +46,30 @@ void fail() {
         cout << a1;
         fin.close();
         fout.close();
+        return 0;
     }
 }
 
 void cons() {
-    cout << "Хорошо, вводите данные. Все числа должны быть в одной строке." << endl;
-    char s1[15]{}, s2[15]{};
+    cout << "Хорошо, вводите данные. Все числа должны быть в одной строке через пробел." << endl;
+    char s1[256]{}, s2[256]{};
     cin >> s1;
     int i{};
+    bool isd{};
+    isd = isdi(s1);
+    if (isd == 1) {
+        cout << "Ошибка ввода. Попробуйте снова." << endl;
+        cons();
+    }
     ofstream fout("1labaout.txt");
     for (i = 0; i < 69; i++) {
         cin >> s2;
+        isd = isdi(s1);
+        while (isd == 1) {
+            cout << "Ошибка ввода. Попробуйте снова." << endl;
+            cin >> s2;
+            isd = isdi(s1);
+        }
         fout << s2;
         fout << " ";
         cout << s2;
@@ -59,17 +93,18 @@ int main() {
     setlocale(LC_ALL, "rus");
     cout << "Вы хотите ввести данные из файла или из консоли?" << endl;
     cout << "0 - из файла, 1 - из консоли." << endl;
-    char a{};
+    char a[256]{};
     cin >> a;
-    if (a == '1') {
+    if (a[0] == '1') {
         cons();
     }
     else {
-        if (a == '0') {
+        if (a[0] == '0') {
             fail();
         }
         else {
-            cout << "Ошибка ввода. Завершаю программу." << endl;
+            cout << "Ошибка ввода. Попробуйте снова." << endl;
+            main();
         }
     }
 }
